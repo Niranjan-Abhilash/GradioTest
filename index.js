959a0cb46a609868b964c14ed3547aac678d353a
Niranjan-Abhilash <125995969+Niranjan-Abhilash@users.noreply.github.com>
2025-04-06 11:50:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,10 @@
 //     document.body.innerHTML = `<h1>${data.greeting}</h1>`;
 //   });
 
-fetch("https://pleasing-expression-production.up.railway.app/")
-  .then(res => res.json())
-  .then(data => {
-    console.log(data.message);
-    await sleep(2000);
-    document.getElementById("greeting").textContent = data.message;
-  });
+// fetch("https://pleasing-expression-production.up.railway.app/")
+//   .then(res => res.json())
+//   .then(data => {
+//     console.log(data.message);
+//     await sleep(2000);
+//     document.getElementById("greeting").textContent = data.message;
+//   });
