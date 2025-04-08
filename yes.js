f992c26e025edf5f84a055349fa873ea33510e07
Niranjan-Abhilash <125995969+Niranjan-Abhilash@users.noreply.github.com>
2025-04-08 14:17:10 +0530

    Update yes.js

--- yes.js
+++ yes.js
@@ -13,14 +13,11 @@ async function main() {
   const greetingElement = document.getElementById("g2E");
   const cursor = document.getElementById("cursor");
 
-  //const response = await fetch("https://pleasing-expression-production.up.railway.app/");
   const response = await fetch("https://shy-marsha-potter-2060b27e.koyeb.app/h");
   const data = await response.json();
-  
-  await typeWriter(data.msg, greetingElement);
 
-  // Blink cursor twice, then hide
-  await sleep(1400); // roughly 2 blinks
+  await typeWriter(data.msg2, greetingElement); // using msg2 since /h endpoint returns that
+  await sleep(1400);
   cursor.style.display = "none";
 }
 
